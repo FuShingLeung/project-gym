63973469797c602acc413bd39767ea9c18d2162f
Fu Shing Leung <bobbyfs123@gmail.com>
2023-04-29 00:56:56 +0100

    updated server validation

--- server/routes/exercises.routes.js
+++ server/routes/exercises.routes.js
@@ -12,15 +12,31 @@ const {
 
 router
   .get('/:id?', getExercises)
-  .post('/', body('exerciseName').trim().isLength({ min: 5 }), (req, res) => {
-    // Finds the validation errors in this request and wraps them in an object with handy functions
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    addExercise(req, res);
-  })
-  .put('/:id', updateExercise)
+  .post(
+    '/',
+    body('exerciseName').trim().isLength({ min: 3 }),
+    body('muscleGroup').trim().isLength({ min: 3 }),
+    (req, res) => {
+      // Finds the validation errors in this request and wraps them in an object with handy functions
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      addExercise(req, res);
+    },
+  )
+  .put(
+    '/:id',
+    body('exerciseName').trim().isLength({ min: 3 }),
+    body('muscleGroup').trim().isLength({ min: 3 }),
+    (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      updateExercise(req, res);
+    },
+  )
   .delete('/:id', removeExercise);
 
 module.exports = router;
