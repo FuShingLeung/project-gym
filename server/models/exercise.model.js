995d1225f4af0910d0482c207bfb3fa31a8d7bb2
Fu Shing Leung <bobbyfs123@gmail.com>
2023-04-29 00:46:28 +0100

    adjusted exerciseName server validation minLength

--- server/models/exercise.model.js
+++ server/models/exercise.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const ExerciseSchema = new Schema({
-  exerciseName: { type: String, required: true, minLength: 5 },
+  exerciseName: { type: String, required: true, minLength: 3 },
   muscleGroup: { type: String, required: true, minLength: 3 },
   avatar_url: {
     type: String,
